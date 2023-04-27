d426ab3f8e2dbcb237cb18b747d1fefc5e6f6a5e
pochita0109 <akosicosta@gmail.com>
2023-04-27 13:07:27 -0700

    First commit, Pseudocodes

---
+++ line_contents.py
@@ -0,0 +1,13 @@
+# Kenneth John Costa
+# Assignment 4
+# Writing multiple line of text contents
+
+# Write a method in python to write multiple line of text contents 
+# into a text file mylife.txt.
+
+# Open a txt file called mylife.txt
+# Ask the user to write a line
+# Ask the user if they want to continue on writing lines
+# If yes:
+# If no:
+# If outside on choices:
