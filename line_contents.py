94e58db63fc7964e3a2674d6dc2bced2d9565f9e
pochita0109 <akosicosta@gmail.com>
2023-04-27 13:23:53 -0700

    Created txt file that appends

--- line_contents.py
+++ line_contents.py
@@ -5,9 +5,12 @@
 # Write a method in python to write multiple line of text contents 
 # into a text file mylife.txt.
 
+def line():
 # Open a txt file called mylife.txt
+    with open("mylife.txt", "a") as output_file: 
 # Ask the user to write a line
 # Write the line entered by user
+     line()
 # Ask the user if they want to continue on writing lines
 # If yes:
 # If no:
