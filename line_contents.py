853d83f7196c700ce1fd18747b2df43f8819ce9e
pochita0109 <akosicosta@gmail.com>
2023-04-27 13:33:07 -0700

    Asked the user to enter the lines

--- line_contents.py
+++ line_contents.py
@@ -9,8 +9,9 @@ def line():
 # Open a txt file called mylife.txt
     with open("mylife.txt", "a") as output_file: 
 # Ask the user to write a line
+     lines = input("\033[93mEnter line:\033[1m ")
 # Write the line entered by user
-     line()
+line()
 # Ask the user if they want to continue on writing lines
 # If yes:
 # If no:
